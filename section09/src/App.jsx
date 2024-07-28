650e7dc516fa7b439c75e6b209bba01441c8958d
kimjiheej <glp0509@naver.com>
2024-07-28 10:48:41 +0900

    useReducer 수정

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -26,54 +26,63 @@ const mockData = [
 ];
 
 function reducer(state, action) {
-      switch (action.type) {
-        case 'CREATE' : return [action.data , ...state]; 
-        case 'UPDATE' : return state.map((item) => item.id === action.targetId? {...item, isDone: !item.isDone}: item); 
-        case 'DELETE' : return state.filter((item) => item.id !== action.targetId)
-        default: 
-        return state; 
-      }
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => item.id !== action.targetId
+      );
+    default:
+      return state;
+  }
 }
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, mockData); 
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  
   const onCreate = (content) => {
-    dispatch ({
-      type : "CREATE", 
-      data : {
-        id : idRef.current++,
-        isDone : false,
-        content : content, 
-        data : new Date().getTime(), 
-      }
-    })
-  }
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
 
-   const onUpdate = (targetId) =>  {
-      dispatch({
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  };
 
-        type: "UPDATE",
-        targetId : targetId 
-      })
-   }
-
-   const onDelete =  (targetId) => {
-
-       dispatch({
-        type: "DELETE", 
-        targetId : targetId,
-       })
-   }
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
 
   return (
     <div className="App">
-      
-     <Header />
+      <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate}  onDelete={onDelete}/> */} */}
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
     </div>
   );
 }
